044e84a5dfba59df97a7517a6b16ec09492f8b40
shubham0794x <shubham07bhadani@gmail.com>
2022-01-23 23:31:58 +0530

    header-component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header_logo"></div>
+
+      <div className="header_search"></div>
+
+      <div className="header_nav">
+        <div className="nav_item">
+          <span className="nav_itemLineOne">Hello Guest</span>
+          <span className="nav_itemLineTwo">Sign In</span>
+        </div>
+        <div className="nav_item">
+          <span className="nav_itemLineOne">Your</span>
+          <span className="nav_itemLineTwo">Shop</span>
+        </div>
+        <div className="nav_item">
+          <ShoppingBasketIcon fontSize="large" />
+          <span className="nav_itemLineTwo">0</span>
+        </div>
+      </div>
     </div>
   );
 }
