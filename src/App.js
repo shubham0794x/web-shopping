8568987c44f3b5ff0793af222f665d66dbb0587a
shubham0794x <shubham07bhadani@gmail.com>
2022-01-25 21:30:05 +0530

    Created Login Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import "./App.css";
+import './App.css';
 import Home from "./Home";
 import Header from "./Header";
 import Checkout from "./Checkout";
-
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./Login";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/checkout">
-          <Header />
-          <Checkout />
-        </Route>
 
-        <Route path="/">
-          <Header />
-          <Home />
-        </Route>
+        <Switch>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+
+          <Route path="/">    
+            <Header />
+            <Home />
+          </Route>
+
+        </Switch>
+
       </Router>
+
     </div>
   );
 }
